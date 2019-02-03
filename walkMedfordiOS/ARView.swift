6e35a589349c126fba7eb88b7201ba6ce9807b9d
user150397 <user150397@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-03 12:00:22 -0500

    Added AR image recognition detection

--- walkMedfordiOS/ARView.swift
+++ walkMedfordiOS/ARView.swift
@@ -7,24 +7,142 @@
 //
 
 import UIKit
+import ARKit
 
 class ARView: UIViewController {
-
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var label: UILabel!
+    
+    let fadeDuration: TimeInterval = 0.3
+    let rotateDuration: TimeInterval = 3
+    let waitDuration: TimeInterval = 0.5
+    
+    lazy var fadeAndSpinAction: SCNAction = {
+        return .sequence([
+            .fadeIn(duration: fadeDuration),
+            .rotateBy(x: 0, y: 0, z: CGFloat.pi * 360 / 180, duration: rotateDuration),
+            .wait(duration: waitDuration),
+            .fadeOut(duration: fadeDuration)
+            ])
+    }()
+    
+    lazy var fadeAction: SCNAction = {
+        return .sequence([
+            .fadeOpacity(by: 0.8, duration: fadeDuration),
+            .wait(duration: waitDuration),
+            .fadeOut(duration: fadeDuration)
+            ])
+    }()
+    
+    lazy var treeNode: SCNNode = {
+        guard let scene = SCNScene(named: "tree.scn"),
+            let node = scene.rootNode.childNode(withName: "tree", recursively: false) else { return SCNNode() }
+        let scaleFactor = 0.005
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x = -.pi / 2
+        return node
+    }()
+    
+    lazy var bookNode: SCNNode = {
+        guard let scene = SCNScene(named: "book.scn"),
+            let node = scene.rootNode.childNode(withName: "book", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.1
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        return node
+    }()
+    
+    lazy var mountainNode: SCNNode = {
+        guard let scene = SCNScene(named: "mountain.scn"),
+            let node = scene.rootNode.childNode(withName: "mountain", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.25
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        node.eulerAngles.x += -.pi / 2
+        return node
+    }()
+    
+    lazy var signNode: SCNNode = {
+        guard let scene = SCNScene(named: "sign.scn"),
+            let node = scene.rootNode.childNode(withName: "sign", recursively: false) else { return SCNNode() }
+        let scaleFactor  = 0.1
+        node.scale = SCNVector3(scaleFactor, scaleFactor, scaleFactor)
+        return node
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sceneView.delegate = self
+        configureLighting()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        resetTrackingConfiguration()
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    @IBAction func resetButtonDidTouch(_ sender: UIBarButtonItem) {
+        resetTrackingConfiguration()
+    }
+    
+    func resetTrackingConfiguration() {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        sceneView.session.run(configuration, options: options)
+        label.text = "Move camera around to detect images"
+    }
+}
 
+extension ARView: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        let referenceImage = imageAnchor.referenceImage
+        let imageName = referenceImage.name ?? "no name"
+        
+        let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.20
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        //planeNode.runAction(imageHighlightAction)
+        node.addChildNode(planeNode)
+        DispatchQueue.main.async {
+            self.label.text = "Image detected: \"\(imageName)\""
+        }
+    }
+    
+    func getPlaneNode(withReferenceImage image: ARReferenceImage) -> SCNNode {
+        let plane = SCNPlane(width: image.physicalSize.width,
+                             height: image.physicalSize.height)
+        let node = SCNNode(geometry: plane)
+        return node
+    }
+    
+    func getNode(withImageName name: String) -> SCNNode {
+        var node = SCNNode()
+        switch name {
+        case "Sign":
+            node = signNode
+        /*case "Snow Mountain":
+            node = mountainNode
+        case "Trees In the Dark":
+            node = treeNode*/
+        default:
+            break
+        }
+        return node
+    }
+    
 }
