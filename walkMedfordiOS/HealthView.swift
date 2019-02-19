4d48ac2c1da251b94c810f6d790c5af98c9900e1
user150397 <user150397@ny021.local>
2019-02-18 19:23:04 -0500

    Started on view for health data

---
+++ walkMedfordiOS/HealthView.swift
@@ -0,0 +1,30 @@
+//
+//  HealthView.swift
+//  walkMedfordiOS
+//
+//  Created by user150397 on 2/18/19.
+//  Copyright © 2019 walkMedford. All rights reserved.
+//
+
+import UIKit
+
+class HealthView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
