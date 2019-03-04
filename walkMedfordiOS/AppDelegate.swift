a803733bac5fb43d502a47d4170441cb5ae9d5d2
samueljayh <hollingsworthsam4@gmail.com>
2019-03-03 19:19:33 -0500

    Added crashlytics

--- walkMedfordiOS/AppDelegate.swift
+++ walkMedfordiOS/AppDelegate.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 walkMedford. All rights reserved.
 //
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         return true
     }
     
