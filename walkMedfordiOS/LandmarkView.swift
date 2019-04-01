727a75ae7876da2b78abeca23296a40c31747ad5
samueljayh <hollingsworthsam4@gmail.com>
2019-04-01 18:44:23 -0400

    Added images for landmarks

--- walkMedfordiOS/LandmarkView.swift
+++ walkMedfordiOS/LandmarkView.swift
@@ -41,8 +41,32 @@ class LandmarkView: UIViewController, MKMapViewDelegate {
         addressLabel.text = landmark.address
         descriptionLabel.text = landmark.description
         
-        centerOnLandmark()
+        setUpImage()
         setUpMap()
+    }
+    
+    /*
+     Purpose: To set up the image
+     Notes:
+     */
+    func setUpImage() {
+        var image = landmark.title.replacingOccurrences(of: " ", with: "_")
+        image = image.replacingOccurrences(of: "/", with: "_")
+        print("Image name: \(image)")
+        imageView.image = UIImage(named: image)
+    }
+    
+    /*
+     Purpose: To set up the map
+     Notes:
+     */
+    func setUpMap() {
+        centerOnLandmark()
+        
+        let landmarkAnnotation = LandmarkAnnotation(title: landmark.title,
+                                                    subtitle: landmark.address,
+                                                    coordinate: landmark.location)
+        self.mapView.addAnnotation(landmarkAnnotation)
     }
     
     /*
@@ -52,19 +76,6 @@ class LandmarkView: UIViewController, MKMapViewDelegate {
     func centerOnLandmark() {
         let coordinateRegion = MKCoordinateRegion.init(center: landmark.location, latitudinalMeters: 2000, longitudinalMeters: 2000)
         mapView.setRegion(coordinateRegion, animated: true)
-    }
-    
-    /*
-     Purpose: To set up the map
-     Notes:
-     */
-    func setUpMap() {
-        let landmarkAnnotation = LandmarkAnnotation(title: landmark.title,
-                                                    subtitle: landmark.address,
-                                                    coordinate: landmark.location)
-        self.mapView.addAnnotation(landmarkAnnotation)
-        
-        
     }
     
     /*
