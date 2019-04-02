4cd1b9883421bdeed8c843443f43f2c5c0ba4e7f
samueljayh <hollingsworthsam4@gmail.com>
2019-04-02 16:09:37 -0400

    Added code for route creation feature

--- walkMedfordiOS/LandmarkCreationView.swift
+++ walkMedfordiOS/LandmarkCreationView.swift
@@ -7,17 +7,128 @@
 //
 
 import UIKit
+import CoreLocation
 
-class LandmarkCreationView: UIViewController {
+class LandmarkCreationView: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     // Variables for input information
-    @IBOutlet weak var landmarkNameText: UITextField!
-    @IBOutlet weak var landmarkAddressText: UITextField!
+    @IBOutlet weak var landmarkName: UITextField!
+    @IBOutlet weak var landmarkDescription: UITextField!
+    @IBOutlet weak var landmarkAddress: UITextField!
     @IBOutlet weak var landmarkImage: UIImageView!
+    var coordinates: CLLocationCoordinate2D!
+    var imagePickerController : UIImagePickerController!
+    var createdLandmark: Landmark!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
+    /*
+     Purpose: To execute when address is completed
+     Notes:
+     */
+    @IBAction func addressCompleted(_ sender: Any) {
+        if (landmarkAddress.text != "") {
+            convertAddress()
+        }
+    }
+    
+    /*
+     Purpose: To convert the street address to latitude and longitude
+     Notes:
+    */
+    func convertAddress() {
+        let streetAddress = landmarkAddress.text as! String
+        let address = "\(streetAddress) Medford, MA, USA"
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address, completionHandler: {(placemarks, error) -> Void in
+            if((error) != nil){
+                print("Error", error as Any)
+            }
+            if let placemark = placemarks?.first {
+                self.coordinates = placemark.location!.coordinate
+            }
+        })
+    }
+    
+    /*
+     Purpose: To add image
+     Notes:
+     */
+    @IBAction func uploadImage(_ sender: Any) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    /*
+     Purpose: To set landmarkImage once photo is chosen
+     Notes:
+     */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imagePickerController.dismiss(animated: true, completion: nil)
+        landmarkImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+    }
+    
+    /*
+     Purpose: To create landmark
+     Notes:
+     */
+    @IBAction func createLandmark(_ sender: Any) {
+        
+        if (allFieldsFilled()) {
+            
+            createdLandmark = Landmark(title: landmarkName.text!,
+                                       latitude: coordinates.latitude,
+                                       longitude: coordinates.longitude,
+                                       address: landmarkAddress.text!,
+                                       description: landmarkDescription.text!)
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    /*
+     Purpose: To check that all fields are filled
+     Notes:
+     */
+    func allFieldsFilled() -> Bool {
+        var filled = true
+        
+        if (landmarkName.text == "") {
+            print("Landmark Name Required")
+            filled = false
+        }
+        
+        if (landmarkDescription.text == "") {
+            print("Landmark Description Required")
+            filled = false
+        }
+        
+        if (landmarkAddress.text == "") {
+            print("Landmark Address Required")
+            filled = false
+        }
+        
+        if (landmarkImage.image == nil) {
+            print("Landmark Image Required")
+            filled = false
+        }
+        
+        return filled
+    }
+    
+    /*
+     Purpose: To pass data to next view
+     Notes:
+     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? RouteCreationView {
+            destinationVC.landmarks.append(createdLandmark)
+            destinationVC.images.append(landmarkImage.image!)
+        }
+    }
 }
--- walkMedfordiOS/RouteCreationView.swift
+++ walkMedfordiOS/RouteCreationView.swift
@@ -9,17 +9,125 @@
 import Foundation
 import UIKit
 
-class RouteCreationView: UIViewController {
+class RouteCreationView: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // Variables for input information
-    @IBOutlet weak var routeNameText: UITextField!
-    @IBOutlet weak var routeDescriptionText: UITextField!
+    @IBOutlet weak var routeName: UITextField!
+    @IBOutlet weak var routeDescription: UITextField!
     @IBOutlet weak var landmarksTable: UITableView!
+    var landmarks = [Landmark]()
+    var images = [UIImage]()
     
+    /*
+     Purpose: To load the variables initially
+     Notes:
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set variables for landmarks tables
+        landmarksTable.dataSource = self
+        landmarksTable.allowsSelection = true
+        landmarksTable.delegate = self
     }
+    
+    /*
+     Purpose: To reload the table once a new landmark is added
+     Notes:
+     */
+    override func viewWillAppear(_ animated: Bool) {
+        landmarksTable.reloadData()
+    }
+    
+    
+    /*
+     Purpose: To specify the number of sections in the table
+     Notes:
+     */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /*
+     Purpose: To specify the number of rows in the table
+     Notes:
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return landmarks.count
+    }
+    
+    /*
+     Purpose: To populate the table with routes and their descriptions
+     Notes:
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier")
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cellIdentifier")
+        }
+        
+        if (tableView == landmarksTable) {
+            cell!.textLabel?.text = landmarks[indexPath.row].title
+            cell!.detailTextLabel?.text = landmarks[indexPath.row].description
+        }
+        
+        return cell!
+    }
+    
+    /*
+     Purpose: To go to landmark creation page if one landmark is chosen to be edited
+     Notes:
+     */
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if (tableView == landmarksTable) {
+            
+        }
+    }
+    
+    /*
+     Purpose: To create a route if all fields are completed
+     Notes:
+     */
+    @IBAction func createRoute(_ sender: Any) {
+        
+        if (allFieldsFilled()) {
+            print("Creating Route")
+        }
+    }
+    
+    /*
+     Purpose: To check if all fields are filled
+     Notes:
+     */
+    func allFieldsFilled() -> Bool {
+        var filled = true
+        
+        if (routeName.text == "") {
+            print("Route Name Required")
+            filled = false
+        }
+        
+        if (routeDescription.text == "") {
+            print("Route Description Required")
+            filled = false
+        }
+        
+        if (landmarks.count < 2) {
+            print("Not Enough Landmarks in Route")
+            filled = false
+        }
+        
+        if (landmarks.count != images.count) {
+            print("Unequal number of landmarks and landmark images")
+            filled = false
+        }
+        
+        return filled
+    }
+    
     
     
     
