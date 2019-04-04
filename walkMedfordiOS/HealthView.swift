7be750a71f1633fd8319b6f7765ce866a99a8c6d
19CraigDN <15Craign@alumni.harker.org>
2019-04-03 20:38:36 -0400

    Made health stats start from when user starts walking a route

--- walkMedfordiOS/HealthView.swift
+++ walkMedfordiOS/HealthView.swift
@@ -58,8 +58,10 @@ class HealthView: UIViewController {
     func getTodaysSteps(completion: @escaping (Double) -> Void) {
         let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
-        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        //let startOfDay = Calendar.current.startOfDay(for: now)
+        if let date = UserDefaults.standard.object(forKey: "date") as? Date {
+            print(date)
+        let predicate = HKQuery.predicateForSamples(withStart: date, end: now, options: .strictStartDate)
         let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
             var resultCount = 0.0
             guard let result = result else {
@@ -71,6 +73,10 @@ class HealthView: UIViewController {
             DispatchQueue.main.async {
                 completion(resultCount) } }
         healthStore.execute(query)
+        }
+        else {
+            self.stepsLabel.text = "You haven't started a route yet."
+        }
     }
     
     @IBAction func showNumberSteps(_ sender: UIButton) {
