a53e0029db56d284cf150ecd9b37c4cf1af66e27
19CraigDN <15Craign@alumni.harker.org>
2019-04-23 18:51:06 -0400

    Made stop button clear map on main view

--- walkMedfordiOS/MapView.swift
+++ walkMedfordiOS/MapView.swift
@@ -425,4 +425,15 @@ class MapView: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
         
         dataTask?.resume()
     }
+    
+    @IBAction func stopRoute(_ sender: Any) {
+        desiredRoute = nil
+        routePolyline = nil
+        directionsToRoutePolyline = nil
+        self.mapView.overlays.forEach {
+            if !($0 is MKUserLocation) {
+                self.mapView.removeOverlay($0)
+            }
+        }
+    }
 }
